8fdf0649abd0dd925c3f93c1ec36d3846086b982
xiewenjun <xiewenjun@g7.com.cn>
2022-04-17 11:33:20 +0800

    feat[solana]: add coupon function

--- packages/solana-pay-tutorial/lib/addresses.ts
+++ packages/solana-pay-tutorial/lib/addresses.ts
@@ -3,3 +3,5 @@ import { PublicKey } from "@solana/web3.js"
 // Your shop wallet address
 export const shopAddress = new PublicKey('9fKmE1itpaWH8f1UCgxYa2VAUbqj9LHBXxY9NwnY2ygR') 
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// TODO: replace with my coupon address
+export const couponAddress = new PublicKey('74aFyMgbKppdab4wEVz5AXL3UGbxMkwkwgK2LVzn3MmG')
