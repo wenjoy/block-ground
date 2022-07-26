9e52940f5329ce20e79b36c83c294497b204a0ab
wenjun <laojiaodaoniu@126.com>
2022-07-26 14:16:25 +0800

    chore[nft-marketplace]: add contract

--- packages/nft-marketplace/migrations/1_deploy_contracts.js
+++ packages/nft-marketplace/migrations/1_deploy_contracts.js
@@ -1,6 +1,9 @@
 
-var SimpleStorage = artifacts.require("SimpleStorage");
+const BoredPetsNFT = artifacts.require('BoredPetsNFT');
+const Marketplace = artifacts.require('Marketplace');
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = async function(deployer) {
+  await deployer.deploy(Marketplace);
+  const marketplace = await Marketplace.deployed(); 
+  await deployer.deploy(BoredPetsNFT, marketplace.address);
 };
